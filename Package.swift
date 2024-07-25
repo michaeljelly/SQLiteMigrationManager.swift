1ce29ee3fbc55daf60122d6e147cb2f3dc3dbcee
michaeljelly <mike@ethi.me>
2024-07-25 13:21:13 +0100

    commit

--- Package.swift
+++ Package.swift
@@ -2,22 +2,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "SQLiteMigrationManager",
-    products: [
-        .library(name: "SQLiteMigrationManager", targets: ["SQLiteMigrationManager"])
-    ],
-    dependencies: [
-        .package(url: "https://github.com/hosy/SQLiteSwift.git", branch: "main")
-    ],
-    targets: [
-        .target(
-            name: "SQLiteMigrationManager",
-            dependencies: ["SQLiteSwift"],
-            path: "Sources",
-            sources: ["SQLiteMigrationManager"]),
-        .testTarget(
-            name: "SQLiteMigrationManagerTests",
-            dependencies: ["SQLiteMigrationManager"],
-            path: "Tests",
-            sources: ["SQLiteMigrationManagerTests.swift"])
-    ])
+  name: "SQLiteMigrationManager",
+  products: [
+    .library(name: "SQLiteMigrationManager", targets: ["SQLiteMigrationManager"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/hosy/SQLiteSwift", .branch("master"))
+  ],
+  targets: [
+    .target(
+      name: "SQLiteMigrationManager",
+      dependencies: ["SQLiteSwift"],
+      path: "Sources",
+      sources: ["SQLiteMigrationManager"]),
+    .testTarget(
+      name: "SQLiteMigrationManagerTests",
+      dependencies: ["SQLiteMigrationManager"],
+      path: "Tests",
+      sources: ["SQLiteMigrationManagerTests.swift"]),
+  ])
